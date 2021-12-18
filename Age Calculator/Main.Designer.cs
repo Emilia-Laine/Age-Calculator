380f09bd2af370d7cbcda85d7e05ae67c8773ea5
Emilia-Laine <94694488+Emilia-Laine@users.noreply.github.com>
2021-12-18 17:29:57 +0000

    Add project files.

---
+++ Age Calculator/Main.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Age_Calculator
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.birthdayPicker = new System.Windows.Forms.DateTimePicker();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // birthdayPicker
+            // 
+            this.birthdayPicker.Location = new System.Drawing.Point(12, 12);
+            this.birthdayPicker.Name = "birthdayPicker";
+            this.birthdayPicker.Size = new System.Drawing.Size(428, 23);
+            this.birthdayPicker.TabIndex = 0;
+            this.birthdayPicker.Value = new System.DateTime(2021, 12, 18, 17, 14, 52, 0);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.calculateButton.Location = new System.Drawing.Point(189, 41);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Calculate!";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 72);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.birthdayPicker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Age Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker birthdayPicker;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ Age Calculator/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Age_Calculator
+{
+    public partial class Main : Form
+    {
+        private int birthdate;
+        private int hours;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            birthdayPicker.Format = DateTimePickerFormat.Custom;
+            birthdayPicker.CustomFormat = "MM/dd/yyyy hh";
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            StoreValues();
+            Calculation();
+        }
+
+        private void StoreValues()
+        {
+            birthdate = birthdayPicker.Value.Date.Year;
+            hours = birthdayPicker.Value.Date.Hour;
+        }
+
+        private void Calculation()
+        {
+            int age = (DateTime.Now.Year - birthdate >= 0) ? DateTime.Now.Year - birthdate : 0;
+            int daysOld = age * 365;
+            int hoursOld = (age > 0) ? DateTime.Now.Minute - hours : 0;
+
+            MessageBox.Show($"{age} years, {daysOld} days and {hoursOld} hours old.");
+        }
+    }
+}
